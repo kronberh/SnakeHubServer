832e14b3a60b4fe265ef583e2204c9ec6c51474b
kronberh <10252213@stud.op.edu.ua>
2024-12-13 10:50:43 +0200

    now for sure

--- SnakeHubServer/Data/ApplicationContext.cs
+++ SnakeHubServer/Data/ApplicationContext.cs
@@ -10,11 +10,5 @@ namespace SnakeHubServer.Data
         {
             Database.EnsureCreated();
         }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            builder.Entity<User>().Property(b => b.TotalGames).HasDefaultValue(0);
-            builder.Entity<User>().Property(b => b.TotalScore).HasDefaultValue(0);
-        }
     }
 }
